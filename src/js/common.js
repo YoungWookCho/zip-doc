e6f8c2599a1b305df73e88b3338952dc8f07d417
impact0823@naver.com <young1596>
2017-01-12 22:03:35 +0900

    오른쪽화살표 완성 픽스드

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,25 @@
 define([
 	"bootstrap",
 ], function() {
-
+	/* 메인로고 마우스오버 이벤트*/
+	$(".header-info-logo").mouseenter( function() {
+		$(".main-logo").attr("src", "img/img_zipdoc_logo_ani.gif");
+	});
+	$(".header-info-logo").mouseleave( function() {
+		$(".main-logo").attr("src", "img/img_zipdoc_logo.png");
+	});
+	/* 디테일스 마우스오버 이벤트*/
+	$(".header-details>ul>li>ul>li").mouseenter( function() {
+		$(this).find(".details-hover").fadeIn(100);
+	});
+	$(".header-details>ul>li>ul>li").mouseleave( function() {
+		$(this).find(".details-hover").fadeOut(100);
+	});
+	/* 메뉴바 마우스오버 이벤트*/
+	$(".header-menu").hover( function() {
+		$(".header-details-wrapper").fadeIn(10);
+	});
+	$(".header-menu").mouseleave( function() {
+		$(".header-details-wrapper").fadeOut(10);
+	});
 });
