24af49523c2e0081247dda053f763e62799b44d4
impact0823@naver.com <young1596>
2017-01-17 18:00:25 +0900

    메인페이지 컨텐츠 마지막 부분 에스크로, 서비스

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 require([
 	"common",
 ], function() {
+	/* 디테일스 마우스오버 이벤트*/
+	function itemEnter() {
+		$(".item").mouseenter( function() {
+			$(this).find(".item-hide").fadeIn(100);
+		});
+	}
+	function itemLeave() {
+		$(".item").mouseleave( function() {
+			$(this).find(".item-hide").fadeOut(100);
+		});
+	}
+	itemEnter();
+	itemLeave();
 });
